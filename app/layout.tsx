5996d8d2e59b935b2cbee5991fcce5bdbda039d8
paolorosepen <p.maniero@roseaupensant.jp>
2024-05-12 14:03:31 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
